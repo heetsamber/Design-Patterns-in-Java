8465e992442da07abb666d40f1426f3dc51cde1f
heetsamber <103236108+heetsamber@users.noreply.github.com>
2022-11-23 09:18:17 +0900

    Delete Observer/ob2 directory

--- Observer/ob2/DisplayElement.java
+++
@@ -1,6 +0,0 @@
-package ob2;
-
-// 보여줄 화면 인터페이스
-public interface DisplayElement {
-    public void display();
-}
--- Observer/ob2/Observer.java
+++
@@ -1,6 +0,0 @@
-package ob2;
-
-// 옵저버 인터페이스
-public interface Observer {
-    public void update(float temperature, float humidity, float pressure);
-}
