d8f4b116f865ada12d524f140fa4503e012edaad
heetsamber <103236108+heetsamber@users.noreply.github.com>
2023-01-09 18:10:24 +0900

    Create EffectiveBuilder.java

---
+++ Builder/EffectiveBuilder/EffectiveBuilder.java
@@ -0,0 +1,77 @@
+package EffectiveBuilder;
+
+// Effective Java의 Builder Pattern
+public class NutritionFacts {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        // Required parameters(필수 인자)
+        private final int servingSize;
+        private final int servings;
+
+        // Optional parameters - initialized to default values(선택적 인자는 기본값으로 초기화)
+        private int calories      = 0;
+        private int fat           = 0;
+        private int carbohydrate  = 0;
+        private int sodium        = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings    = servings;
+        }
+
+        public Builder calories(int val) {
+            calories = val;
+            return this;    // 이렇게 하면 . 으로 체인을 이어갈 수 있다.
+        }
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+        public Builder carbohydrate(int val) {
+            carbohydrate = val;
+            return this;
+        }
+        public Builder sodium(int val) {
+            sodium = val;
+            return this;
+        }
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+        servingSize  = builder.servingSize;
+        servings     = builder.servings;
+        calories     = builder.calories;
+        fat          = builder.fat;
+        sodium       = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+}
+
+class Program{
+    public static void main(String[] args){
+        NutritionFacts.Builder builder = new NutritionFacts.Builder(200, 8);
+        builder.calories(100);
+        builder.sodium(30);
+        builder.carbohydrate(20);
+        
+        NutritionFacts cocaCola = builder.build();
+        
+        
+        NutritionFacts cocaCola2 = new NutritionFacts
+                                        .Builder(100,5)
+                                        .calories(150)
+                                        .sodium(50)
+                                        .carbohydrate(22)
+                                        .build();
+                
+    }
+}
