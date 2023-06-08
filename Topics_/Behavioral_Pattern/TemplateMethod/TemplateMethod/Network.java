69e4b77cb1777002dfeb45e9d924a6e38f277420
heetsamber <103236108+heetsamber@users.noreply.github.com>
2023-06-08 23:50:23 +0900

    Add files via upload

---
+++ Topics_/Behavioral_Pattern/TemplateMethod/TemplateMethod/Network.java
@@ -0,0 +1,21 @@
+package TemplateMethod;
+
+public abstract class Network {
+    String userName;
+    String password;
+
+    Network() {}
+
+    public boolean post(String message){
+        if(logIn(this.userName, this.password)){
+            boolean result = sendData(message.getBytes());
+            logOut();
+            return result;
+        }
+        return false;
+    }
+
+    abstract boolean logIn(String userName, String password);
+    abstract boolean sendData(byte[] data);
+    abstract void logOut();
+}
