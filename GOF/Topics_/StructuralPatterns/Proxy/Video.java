7561d7cfa553cb60a5addc0e8efe5917c81b19f3
heetsamber <103236108+heetsamber@users.noreply.github.com>
2023-08-17 20:11:34 +0900

    Add files via upload

---
+++ GOF/Topics_/StructuralPatterns/Proxy/Video.java
@@ -0,0 +1,13 @@
+package Proxy;
+
+public class Video {
+    public String id;
+    public String title;
+    public String data;
+
+    Video(String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.data = "Random video.";
+    }
+}
